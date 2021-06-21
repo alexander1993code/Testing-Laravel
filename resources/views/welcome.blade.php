51810ed569cc737d0cf904f81ce0850b3faf3719
alexandercode1993 <alexandersala2015@gmail.com>
2021-06-21 16:26:34 -0300

    creacion de etiqueta y eliminar etiquetas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,11 +7,25 @@
     <title>Document</title>
 </head>
 <body>
+    <form action="tags" method="POST">
+        @csrf 
+        <input type="text" name="name">
+        <input type="submit" value="Agregar">
+    </form>
     <h4>Listado de etiquetas</h4>
     <table>
         @forelse($tags as $tag)
             <tr>
                 <td> {{ $tag->name }} </td>
+                <td>
+                    <form action="tags/{{ $tag->id }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+
+                        <input type="submit" value="Delete">
+
+                    </form>
+                </td>
             </tr>
         @empty
             <tr>
